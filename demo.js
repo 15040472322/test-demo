d9ee4790a3e0533c9638b656e92ac6b9d3f75fc3
李佳泽 <lijiaze@lijiazedeiMac.local>
2022-03-11 09:28:19 +0800

    提交后重新进行测试

--- demo.js
+++ demo.js
@@ -2,3 +2,5 @@ var demo = "test-demo";
 console.log(demo);
 
 var demo1 = "再一次改动";
+var demo12 = "3.11第一次测试改动";
+var demo13 = "3.11第二次测试改动";
---
+++ demo3-2.js
@@ -0,0 +1,4 @@
+var demo = "test-demo";
+console.log(demo);
+
+var demo1 = "再一次改动";
